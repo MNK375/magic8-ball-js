77b14b4bd9fdee7caa6c04820b46c149ed8b3d79
MINAKO.P <69062635+MNK375@users.noreply.github.com>
2020-11-17 16:02:51 -0700

    DONE

--- script.js
+++ script.js
@@ -8,28 +8,26 @@ $(document).ready(function(){
       var answer = this.listOfAnswers[randomIndex];
 
       $("#answer").text( answer );
-
+     
       console.log(question);
       console.log(answer);
   };  
-       $("#answer").hide();
-
-        $("#answer").hide();    
-        
-      var onClick = function() {  
-       var question = prompt("Will today be a great day? ASK A YES/NO QUESTION!");   
-      magic8Ball.askQuestion(question); 
+       $("#answer").hide();       
      
-      $("#8ball").effect( "shake" );        
-      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
-
-      $("#answer").fadeIn(4000);
+var onClick = function() {
+        $("#answer").hide();  
+          
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");        
+              
       setTimeout(
      function() {
          //show prompt
      magic8Ball.askQuestion(question);
      }, 500);
-     };
-     $("#questionButton").click( onClick );
-   //wait half a second before showing prompt
-});
+     $("#8ball").effect( "shake" );      
+      $("#answer").fadeIn(4000);
+     var question = prompt("Will today be a great day? ASK A YES/NO QUESTION!");    
+    }
+    $("#questionButton").click( onClick);
+   //wait half a seond before showing prompt
+  });
